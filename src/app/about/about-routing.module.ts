6df427640eb4554419fe2a3f9db483049871535b
Daniel Ponce <daniel.ponce@ALTIAVALLADOLID.LOCAL>
2019-03-01 14:15:09 +0100

    Added heroes to navbar

---
+++ src/app/about/about-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { InfoComponent } from './about/info/info.component';
+import { LinksComponent } from './about/links/links.component';
+import { AuthorsComponent } from './about/authors/authors.component';
+import { AuthorComponent } from './about/authors/author/author.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent,
+    children: [
+      {
+        path: 'info',
+        component: InfoComponent
+      },
+      {
+        path: 'links',
+        component: LinksComponent
+      },
+      {
+        path: 'authors',
+        component: AuthorsComponent
+      },
+      {
+        path: 'authors/:id',
+        component: AuthorComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AboutRoutingModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './home/home.module#HomeModule'
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'about',
+    loadChildren: './about/about.module#AboutModule'
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
